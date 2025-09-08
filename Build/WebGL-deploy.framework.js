bad75028a1dd22d7313a219ab5470708f44a9dd6
namtd <namtd@arrow-tech.vn>
2025-09-08 16:45:16 +0700

    update

--- Build/WebGL-deploy.framework.js
+++ Build/WebGL-deploy.framework.js
@@ -1126,7 +1126,7 @@ function createWasm() {
     // This assertion doesn't hold when emscripten is run in --post-link
     // mode.
     // TODO(sbc): Read INITIAL_MEMORY out of the wasm file in post-link mode.
-    //assert(wasmMemory.buffer.byteLength === 33554432);
+    //assert(wasmMemory.buffer.byteLength === 268435456);
     updateMemoryViews();
 
     wasmTable = Module['asm']['__indirect_function_table'];
