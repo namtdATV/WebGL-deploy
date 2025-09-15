adca8e239275a3167cf2524bf390fb244295bfa7
namtd <namtd@arrow-tech.vn>
2025-09-15 15:37:14 +0700

    update

--- Build/Wakachiku.loader.js
+++ Build/Wakachiku.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 75713170,
+    wasmFileSize: 75713205,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
