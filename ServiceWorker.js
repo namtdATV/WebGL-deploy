f9449348ebaf25def87c39703feb438ca55cd034
namtd <namtd@arrow-tech.vn>
2025-09-16 11:18:46 +0700

    update br

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "tac-wakachiku-0.1";
 const contentToCache = [
     "Build/Wakachiku.loader.js",
-    "Build/Wakachiku.framework.js",
-    "Build/Wakachiku.data",
-    "Build/Wakachiku.wasm",
+    "Build/Wakachiku.framework.js.br",
+    "Build/Wakachiku.data.br",
+    "Build/Wakachiku.wasm.br",
     "TemplateData/style.css"
 
 ];
